254f41086a56c593d0911326408a04d1a373ba46
Ajay Kumar <ajay_kumar@elitemindz.co>
2026-05-26 13:53:49 +0530

    added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+require("./modules/auth/router/auth.router")(app);
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-require("./modules/auth/router/auth.router")(app);
 
 module.exports = app;
--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -1,17 +1,31 @@
-const AuthModel = require("../model/auth.model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const AuthModel = require("../model/auth.model");
 
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const existingUser = await AuthModel.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
+    if (!existingUser) {
+      return res.status(400).json({ message: "Invalid email or password" });
     } else {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = new AuthModel({ email, password: hashedPassword });
-      await newUser.save();
-      return res.status(201).json({ message: "User registered successfully" });
+      const isMatch = await bcrypt.compare(password, existingUser.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign(
+        {
+          id: existingUser._id,
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" },
+      );
+
+      return res.status(200).json({ message: "Login successfully", token });
     }
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
