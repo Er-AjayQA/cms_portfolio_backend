19bb0a7d80e042dd5b50ab305147e35eb536e5d4
Ajay Kumar <ajay_kumar@elitemindz.co>
2026-05-26 16:21:50 +0530

    added

--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -25,7 +25,14 @@ exports.login = async (req, res) => {
         { expiresIn: "1h" },
       );
 
-      return res.status(200).json({ message: "Login successfully", token });
+      return res.status(200).json({
+        message: "Login successfully",
+        token,
+        user: {
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+      });
     }
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
